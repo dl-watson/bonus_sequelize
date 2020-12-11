563c4fa60fb3e525e435401181edbb6b89783e54
dl-watson <dlarkinwatson@gmail.com>
2020-12-11 08:15:56 -0800

    post test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const request = require("supertest");
 const app = require("../lib/app");
 const sequelize = require("../database/utils/sequelize");
+const Toys = require("../database/models/Toys");
 
 describe("bonus_sequelize routes", () => {
   beforeEach(() => {
@@ -12,7 +13,18 @@ describe("bonus_sequelize routes", () => {
     return sequelize.end();
   });
 
-  it("posts a new toy", async () => {});
+  it("posts a new toy", async () => {
+    const res = await request(app)
+      .post("/api/v1/toys")
+      .send({ color: "blue", name: "sailboat" });
+
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        color: "blue",
+        name: "sailboat",
+      })
+    );
+  });
   it("gets all toys", async () => {});
   it("gets a toy by id", async () => {});
   it("updates a toy by id", async () => {});
