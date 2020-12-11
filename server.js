ca347d229f1272379685fdd753ab03222019526d
dl-watson <dlarkinwatson@gmail.com>
2020-12-10 23:31:57 -0800

    cleaning up for submission

--- server.js
+++ server.js
@@ -5,25 +5,20 @@ const PORT = process.env.PORT || 7890;
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
     console.log(`Started on ${PORT}`);
   });
 });
 
-// Tests the sequelize connection
 sequelize
   .authenticate()
   .then((err) => {
-    // eslint-disable-next-line no-console
     console.log("Connection has been established successfully.");
   })
   .catch((err) => {
-    // eslint-disable-next-line no-console
     console.log("Unable to connect to the database:", err);
   });
 
 process.on("exit", () => {
-  // eslint-disable-next-line no-console
   console.log("Goodbye!");
   sequelize.end();
 });
