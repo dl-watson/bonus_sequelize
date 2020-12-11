bebdd836e0fd27a227bc7ce9268b76c891b56eeb
dl-watson <dlarkinwatson@gmail.com>
2020-12-11 08:46:00 -0800

    get toy by id test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const request = require("supertest");
 const app = require("../lib/app");
 const sequelize = require("../database/utils/sequelize");
-const Toys = require("../database/models/Toys");
+const { Toys } = require("../database/models/Toys");
 
 describe("bonus_sequelize routes", () => {
   beforeEach(() => {
@@ -25,8 +25,50 @@ describe("bonus_sequelize routes", () => {
       })
     );
   });
-  it("gets all toys", async () => {});
-  it("gets a toy by id", async () => {});
+
+  it("gets all toys", async () => {
+    const toys = await Promise.all(
+      [
+        {
+          color: "red",
+          name: "truck",
+        },
+        {
+          color: "yellow",
+          name: "ducky",
+        },
+        {
+          color: "green",
+          name: "block",
+        },
+      ].map((toy) => Toys.create(toy))
+    );
+    const res = await request(app).get("/api/v1/toys");
+
+    // oh my god, the fucking updatedAt is returning as a date datatype, where jest is expecting a string datatype
+    expect(res.body).toEqual(expect.arrayContaining(toys));
+  });
+
+  it("gets a toy by id", async () => {
+    const newToy = await Toys.create({
+      color: "red",
+      name: "truck",
+    });
+
+    const toy = await Toys.findByPk(newToy.id);
+
+    const res = await request(app).get(`/api/v1/toys/${toy.id}`);
+
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        color: "red",
+        createdAt: expect.anything(),
+        id: 1,
+        name: "truck",
+        updatedAt: expect.anything(),
+      })
+    );
+  });
   it("updates a toy by id", async () => {});
   it("deletes a toy by id", async () => {});
 });
